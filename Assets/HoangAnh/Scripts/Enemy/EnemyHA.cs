3e6d87e9543c0b2e4487b8797cbd8b8a3cca7dec
hoanganh-unity-0711 <hoanganhunity@gmail.com>
2025-06-03 22:17:51 +0700

    Update

--- Assets/HoangAnh/Scripts/Enemy/EnemyHA.cs
+++ Assets/HoangAnh/Scripts/Enemy/EnemyHA.cs
@@ -7,6 +7,9 @@ namespace HoangAnh
 {
     public class EnemyHA : MonoBehaviour
     {
+        [Space,Header("Animation")]
+        [SerializeField] Animator _animator;
+        
         public List<TitleMapHA> ListPath = new List<TitleMapHA>();
         public float speed = 1;
         public void SetupData(List<TitleMapHA> ListPath)
@@ -19,10 +22,22 @@ namespace HoangAnh
             if (ListPath.Count > 0)
             {
                 TitleMapHA titleTarget = ListPath[0];
-                Vector3 positionTarget = new Vector3(titleTarget.transform.position.x, transform.position.y, titleTarget.transform.position.z);
+                Vector3 posTarget = titleTarget.transform.position;
+                if (ListPath.Count == 1)
+                {
+                    posTarget.x += 2f;
+                }
+                Vector3 positionTarget = new Vector3(posTarget.x, transform.position.y, posTarget.z);
                 float distance = Vector3.Distance(transform.position, positionTarget);
                 if (distance > 0.05f)
                 {
+                    Vector3 direction = (positionTarget - transform.position).normalized;
+                    if (direction != Vector3.zero) 
+                    {
+                        Quaternion lookRotation = Quaternion.LookRotation(direction);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 20f); 
+                    }
+                    _animator.SetBool("isRun", true);
                     transform.position = Vector3.MoveTowards(transform.position, positionTarget, speed * Time.deltaTime);
                 }
                 else
@@ -30,6 +45,7 @@ namespace HoangAnh
                     ListPath.RemoveAt(0);
                     if (ListPath.Count <= 0)
                     {
+                        _animator.SetBool("isRun", false);
                         Debug.Log("Thua");
                     }
                 }
